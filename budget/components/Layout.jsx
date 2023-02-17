94a625830affef7c6a1868b89548c93c77e1fcda
techakhil-me <techakhilc47@gmail.com>
2023-02-17 22:17:11 +0530

    home ui

--- budget/components/Layout.jsx
+++ budget/components/Layout.jsx
@@ -13,30 +13,30 @@ const Layout = ({ children }) => {
       <main className="w-full h-full p-5">{children}</main>
       <div className="flex w-full p-5 pb-2 items-center justify-between sticky bottom-0">
         <div className="p-2">
-          <div className="flex flex-col w-12 h-12  text-center items-center">
+          <div className="flex flex-col w-12 h-12  text-xs text-center items-center">
             <RiHome2Line size={24} />
             Home
           </div>
         </div>
         <div className="p-2">
-          <div className="flex flex-col w-12 h-12  text-center items-center">
+          <div className="flex flex-col w-12 h-12 text-xs  text-center items-center">
             <RiPieChart2Line size={24} />
             Expenses
           </div>
         </div>
-        <div className="p-2 bg-black text-white rounded-full -translate-y-4">
+        <div className="p-2 bg-black text-white text-xs rounded-full -translate-y-4">
           <div className="flex flex-col w-12 h-12  text-center items-center justify-center">
             <RiBankCard2Fill size={24} />
           </div>
         </div>
         <div className="p-2">
-          <div className="flex flex-col w-12 h-12  text-center items-center">
+          <div className="flex flex-col w-12 h-12 text-xs text-center items-center">
             <RiBarChart2Line size={24} />
             Invest
           </div>
         </div>
         <div className="p-2">
-          <div className="flex flex-col w-12 h-12  text-center items-center">
+          <div className="flex flex-col w-12 h-12 text-xs text-center items-center">
             <RiCalendarCheckLine size={24} />
             Goals
           </div>
